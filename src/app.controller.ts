176e4a21bece12a4acdf31008c04b6ebd5e300cc
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-12-17 15:45:29 -0300

    feat: creating post route  to create a notification

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { randomUUID } from 'node:crypto';
 
 @Controller('notifications')
 export class AppController {
   constructor(private readonly prisma: PrismaService) {}
 
   @Get()
-  getNotifications() {
+  list() {
     return this.prisma.notification.findMany();
+  }
+
+  @Post()
+  async create(@Body() body: any) {
+    const { recipientId, content, category } = body;
+
+    await this.prisma.notification.create({
+      data: {
+        id: randomUUID(),
+        content,
+        category,
+        recipientId,
+      },
+    });
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { MailService } from './mail/mail.service';
-import { SMTPMailService } from './mail/smtp.service';
+
 import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [
-    PrismaService,
-    {
-      provide: MailService,
-      useClass: SMTPMailService,
-    },
-  ],
+  providers: [PrismaService],
 })
 export class AppModule {}
