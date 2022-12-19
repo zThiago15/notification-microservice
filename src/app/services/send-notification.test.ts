dbaca063b39b14396e8082c33bd00a16dfeb88d0
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-12-18 23:35:08 -0300

    feat: creating in-memory database for tests

--- src/app/services/send-notification.test.ts
+++ src/app/services/send-notification.test.ts
@@ -1,16 +1,9 @@
-import { Notification } from '../entities/notification';
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repository.test';
 import { SendNotification } from './send-notification';
-
-const notifications: Notification[] = [];
-
-const notificationsRepository = {
-  async create(notification: Notification) {
-    notifications.push(notification);
-  },
-};
 
 describe.only('send notification', () => {
   it('should be able to send a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
     const sendNotification = new SendNotification(notificationsRepository);
 
     const { notification } = await sendNotification.execute({
@@ -20,6 +13,7 @@ describe.only('send notification', () => {
     });
 
     expect(notification).toBeTruthy();
-    expect(notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications).toHaveLength(1);
+    expect(notificationsRepository.notifications[0]).toEqual(notification);
   });
 });
