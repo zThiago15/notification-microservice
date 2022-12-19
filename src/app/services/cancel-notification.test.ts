6ddca4304c8f5001e46e9cdd4fcc6607015fa32c
Thiago Dias <thiaguinhodias.15@gmail.com>
2022-12-19 01:33:34 -0300

    test: throw error when cancelling a non-existing notification

--- src/app/services/cancel-notification.test.ts
+++ src/app/services/cancel-notification.test.ts
@@ -3,7 +3,7 @@ import { Content } from '@app/entities/validations/content';
 import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
 import { CancelNotification } from './cancel-notification';
 
-describe.only('Cancel notification', () => {
+describe('Cancel notification', () => {
   it('should be able to cancel a notification', async () => {
     const notificationsRepository = new InMemoryNotificationsRepository();
     const cancelNotification = new CancelNotification(notificationsRepository);
@@ -23,5 +23,16 @@ describe.only('Cancel notification', () => {
     expect(notificationsRepository.notifications[0].canceledAt).toEqual(
       expect.any(Date),
     );
+  });
+
+  it('it should not be able to cancel a non-existing notification ', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const cancelNotification = new CancelNotification(notificationsRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrowError('Notification not found');
   });
 });
